8299f7efac0555be5cd02371dd4d1b9a4b0188e8
Boogyeom Kim <153355571+b00kkk@users.noreply.github.com>
2025-01-16 18:07:19 +0900

    코드 설명 추가

--- Part2/log_rdd_ex.py
+++ Part2/log_rdd_ex.py
@@ -104,7 +104,28 @@ if __name__ == '__main__':
         .reduceByKey(lambda i1, i2: f"{i1},{i2}")\
         .map(lambda  row: (row[0], row[1].split(",")))
 
-    result2.foreach(print)
+    result2.collect()
 
     # group by 가 reduceByKey 보다 가독성이 좋다.
     # 하지만 성능은 reduceByKey가 더 좋음
+
+    while True:
+        pass
+
+    # localhost:4040/jobs/를 들어가 Spark UI 확인
+    # Jobs는 action 연산에 의해 구분이 됨
+    # action이 collct 하나만 있기에 현재 action도 1개
+    # 위치를 보면 107번 째 줄에 action이 있다는 것을 확인할 수 있음
+    # 들어가 보면 두 개의 stage로 나누어져 있는 것을 확인할 수 있음
+    # 첫 번째 stage는 File을 읽어오는 부분, 두 번째 stage에서 데이터 교환이 발생함
+    # 첫 번째와 두 번째를 가르는 부분이 104번 쨰 줄이라는 것을 확인할 수 있음
+    # reduceByKey가 narrow transformation이 아닌 wide transformation임
+    # 여기서 실행기(executor)간에 데이터 교환이 발생함
+    # 이것을 "셔플"이라고 부름
+    # 스테이지를 가르는 기준이 셔플의 발생 여부이다.
+    # Event Timeline을 보면 언제 Executor가 추가되고 연산이 얼마나 걸렸는지 확인이 가능함
+    # details를 클릭해 코드 실행 로그를 확인할 수 있음
+    # Storage는 현재 나오지 않지만, Spark의 중간 결과를 캐싱하는 경우 나타남
+    # Environment는 Spark의 Config들이 나타남
+    # Executors에서는 각각의 로그를 확인해 볼 수 있음
+    # SparkSQL을 사용하면 UI에서 SQL탭을 확인할 수 있
